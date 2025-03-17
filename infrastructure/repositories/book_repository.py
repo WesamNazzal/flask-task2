8c489b062da6d1674e57d12565034aa4bfe4b5b1
WesamNazzal <wesam.nazzal@sociumtech.com>
2025-03-17 15:37:56 +0300

    f

--- infrastructure/repositories/book_repository.py
+++ infrastructure/repositories/book_repository.py
@@ -1,17 +1,18 @@
 from typing import Any, Dict, List
 from sqlalchemy.sql import select
+from sqlalchemy.sql.schema import Table
 from infrastructure.database.schema.schema import books, members
 from infrastructure.repositories.shared.base_repository import BaseRepository
 from infrastructure.repositories.unit_of_work import UnitOfWork
 
 
-class BookRepository(BaseRepository[books]):
+class BookRepository(BaseRepository[Table]):
     def __init__(self) -> None:
         super().__init__(books)
 
     def get_books_by_author(self, author: str) -> List[Dict[str, Any]]:
         with UnitOfWork() as uow:
-            result = uow.session.execute(
+            result = uow.connection.execute(
                 select(self.table).where(self.table.c.author == author)
             )
             return [dict(row) for row in result.mappings().all()] if result else []
@@ -19,14 +20,14 @@ class BookRepository(BaseRepository[books]):
     def is_book_borrowed(self, book_id: int) -> bool:
         with UnitOfWork() as uow:
             return bool(
-                uow.session.execute(
+                uow.connection.execute(
                     select(self.table.c.is_borrowed).where(self.table.c.book_id == book_id)
                 ).scalar()
             )
 
     def get_borrowed_book_with_member(self, book_id: int) -> Dict[str, Any] | None:
         with UnitOfWork() as uow:
-            result = uow.session.execute(
+            result = uow.connection.execute(
                 select(
                     books.c.book_id,
                     books.c.title,
